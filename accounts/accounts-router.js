702b72262394c1a3d1580176362c9137293ffe72
roselandroche <r.landroche@gmail.com>
2019-12-18 19:35:29 -0500

    Put and delete functional

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -35,4 +35,28 @@ router.post('/', async (req, res, next) => {
     }
 })
 
+router.put('/:id', async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db('accounts').where('id', req.params.id).update(payload)
+        res.json(await db('accounts').where('id', req.params.id).first())
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await db('accounts').where('id', req.params.id).del()
+        res.status(204).end()
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
